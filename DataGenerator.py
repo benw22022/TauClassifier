NOW I have Fixed up code so that tf.data datasets now function correctly (I was a muppet in my last commit). Added pT re-weighting

DataGenerator.py
@@ -18,7 +18,7 @@ import time
 
 class DataGenerator(keras.utils.Sequence):
 
-    def __init__(self, file_dict, variables_dict, nbatches=1000, cuts=None, label="DataGenerator"):
+    def __init__(self, file_dict, variables_dict, nbatches=1000, epochs=50, cuts=None, label="DataGenerator"):
         """
         Class constructor - loads batches of data in a way that can be fed one by one to Keras - avoids having to load
         entire dataset into memory prior to training
@@ -28,8 +28,8 @@ class DataGenerator(keras.utils.Sequence):
         and values being a list of branch names of the variables associated with that type
         :param nbatches: Number of batches to *roughly* split the dataset into (not exact due to uproot inconstantly
         changing batch size when moving from one file to another)
-        :param cuts: A dictionary of cuts with keys the same as file_dict. The values should be a string or list of
-        strings which can be parsed by uproot.lazy's cut options
+        :param cuts: A dictionary of cuts with keys the same as file_dict. The values should be a string which can be
+        parsed by uproot's cut option e.g. "(pt1 > 50) & ((E1>100) | (E1<90))"
         :param label: A string to label the generator with - useful when debugging multiple generators
         """
         logger.log("Initializing DataGenerator", 'INFO')
@@ -38,6 +38,9 @@ class DataGenerator(keras.utils.Sequence):
         self.data_loaders = []
         self.cuts = cuts
         self._current_index = 0
+        self._epochs = epochs
+        self._epoch_index = 0
+
 
         # Organise a list of all variables
         self._variables_dict = variables_dict
@@ -77,7 +80,7 @@ class DataGenerator(keras.utils.Sequence):
         logger.log("DataGenerator initialized", 'INFO')
 
 
-    def load_batch(self, idx=0):
+    def load_batch(self, idx=None):
         """
         Loads a batch of data from each data type and concatenates them into single arrays for training
         :param idx: The index of the batch of data to retrieve
@@ -86,7 +89,11 @@ class DataGenerator(keras.utils.Sequence):
 
         batch_load_time = time.time()
 
-        batch = [dl.load_batch_from_data(idx=idx) for dl in self.data_loaders]
+        index = idx
+        if idx is None:
+            index = self._current_index
+
+        batch = [dl.load_batch_from_data(idx=index) for dl in self.data_loaders]
         track_array = np.concatenate([sub_batch[0][0] for sub_batch in batch])
         neutral_pfo_array = np.concatenate([sub_batch[0][1] for sub_batch in batch])
         shot_pfo_array = np.concatenate([sub_batch[0][2] for sub_batch in batch])
@@ -95,7 +102,7 @@ class DataGenerator(keras.utils.Sequence):
         label_array = np.concatenate([sub_batch[1] for sub_batch in batch])
         weight_array = np.concatenate([sub_batch[2] for sub_batch in batch])
 
-        logger.log(f"Loaded batch {self.label} {self._current_index}/{self.__len__()} in {str(datetime.timedelta(seconds=time.time()-batch_load_time))}", "DEBUG")
+        logger.log(f"Loaded batch {self.label} {self._current_index}/{self.__len__()} in {str(datetime.timedelta(seconds=time.time()-batch_load_time))}", "INFO")
         logger.log(f"Batch: {self._current_index}/{self.__len__()} - shapes:", 'DEBUG')
         logger.log(f"TauTracks Shape = {track_array.shape}", 'DEBUG')
         logger.log(f"ConvTracks Shape = {conv_track_array.shape}", 'DEBUG')
@@ -140,7 +147,7 @@ class DataGenerator(keras.utils.Sequence):
         run into memory access violations when Keras oversteps bounds of array)
         :return: The number of batches in an epoch
         """
-        return self._num_batches - 1
+        return self._num_batches
 
     def __getitem__(self, idx):
         """
@@ -148,19 +155,31 @@ class DataGenerator(keras.utils.Sequence):
         :param idx: An index
         :return: A full batch of data
         """
-        logger.log(f"loaded batch {idx}/{self.__len__()}", 'DEBUG')
+        #logger.log(f"loaded batch {idx}/{self.__len__()}", 'DEBUG')
+        logger.log(f"{self.label} __getitem__ called")
         self._current_index += 1
         return self.load_batch(idx)
 
 
     def __next__(self):
         """
-        Overloads next() operator - allows generator to be iterable
+        Overloads next() operator - allows generator to be iterable - This is what will be called when
+        tf.data.Dataset.from_generator() is called
         :return:
         """
-        if self._current_index < self._num_batches:
-            self._current_index += 1
-            return self.load_batch(0)
+
+        #if self._current_index < self._num_batches:
+        #    self._current_index += 1
+        #    return self.load_batch(0)
+        logger.log(f"{self.label} __next__ called")
+        if self._epoch_index < self._epochs:
+            if self._current_index < self._num_batches:
+                self._current_index += 1
+                return self.load_batch()
+            self.on_epoch_end()
+            self._epoch_index += 1
+            logger.log(f"Completed {self._epoch_index} epochs")
+            return self.load_batch()
         raise StopIteration
 
     def __iter__(self):
@@ -180,6 +199,7 @@ class DataGenerator(keras.utils.Sequence):
         for data_loader in self.data_loaders:
             #data_loader.set_batches(self._variables_list)
             data_loader.reset_index()
+        logger.log(f"reset_generator called on {self.label}")
 
     def on_epoch_end(self):
         """

DataLoader.py
@@ -9,15 +9,18 @@ import math
 import awkward as ak
 import numpy as np
 import uproot
-from preprocessing import finite_log
+from preprocessing import finite_log, pt_reweight
 from utils import logger
 from variables import log_list
 
 
 def apply_logs(np_arrays, variables, log_list):
-    for i in range(0, len(variables)):
-        if variables[i] in log_list:
-            np_arrays[:, i] = np.log(np_arrays[:, i], out=np.zeros_like(np_arrays[:, i]), where=(np_arrays[:, i] > 1))
+    #for i in range(0, len(variables)):
+    #    if variables[i] in log_list:
+    # Take logs of everything where the mean of the column is > 1
+    for i in range(0, np_arrays.shape[1]):
+        if np.mean(np_arrays[:, i]) > 1:
+            np_arrays[:, i] = np.log(np_arrays[:, i], out=np.zeros_like(np_arrays[:, i]), where=(np_arrays[:, i] > 0))
     return np_arrays
 
 
@@ -35,7 +38,7 @@ class DataLoader:
         :param nbatches: number of batches to *roughly* split the data into
         :param dummy_var: A variable to be loaded from the file to be loaded and iterated through to work out the number
         of events in the data files.
-        :param cuts: A string or list of strings that can be parsed by uproot.lazy to apply cuts to the data
+        :param cuts: A string which can be parsed by uproot's cut option e.g. "(pt1 > 50) & ((E1>100) | (E1<90))"
         :param batch_size: Allows you to manually set the batch size for the data. This will override the automatically
         calculated batch size inferred from nbatches
         """
@@ -170,7 +173,9 @@ class DataLoader:
                 else:
                     labels_np_array[:, 3] = 1
 
-        weight_np_array = ak.to_numpy(batch[self._variables_dict["Weight"]]).astype("float32")
+        # weight_np_array = ak.to_numpy(batch[self._variables_dict["Weight"]]).astype("float32")
+        # Apply pT re-weighting
+        weight_np_array = pt_reweight(ak.to_numpy(batch[self._variables_dict["TauJets.jet_pt"]]).astype("float32"))
 
         logger.log(f"Loaded batch {self._current_index} from {self._data_type}: {self.label}", "DEBUG")
 
@@ -210,6 +215,9 @@ class DataLoader:
     def reset_index(self):
         self._current_index = 0
         self._disable_indexing = False
+        self._batches_generator = None
+        self._batches_generator = uproot.lazy(self.files, filter_name=self._variables_list, cut=self.cut,
+                                              step_size=int(self.specific_batch_size * 1.5))
 
     def num_events(self):
         return self._num_events

main.py
@@ -13,6 +13,7 @@ from callbacks import ParallelModelCheckpoint, TimingCallback
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from utils import logger
 import tensorflow as tf
+import pickle
 from config import config_dict
 logger.set_log_level('INFO')
 
@@ -59,7 +60,7 @@ def main():
     train_dataset = tf.data.Dataset.from_generator(training_batch_generator, output_types=types, output_shapes=shapes)
     train_dataset = tf.data.Dataset.range(2).interleave(lambda _: train_dataset, num_parallel_calls=12,)
     train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
-    train_dataset = train_dataset.repeat(100)
+    #train_dataset = train_dataset.repeat(100)
 
     validation_batch_generator = DataGenerator(validation_files_dictionary, variables_dictionary, nbatches=250, cuts=cuts,
                                                label="Validation Generator")
@@ -68,9 +69,9 @@ def main():
     val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
     #val_dataset  =val_dataset.repeat()
 
-    #==================================================================================================================#
-    # Initialize Model
-    #==================================================================================================================#
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Initialize Model
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     model = ModelDSNN(config_dict)
 
     # Configure callbacks
@@ -86,26 +87,23 @@ def main():
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4, patience=3, min_lr=4e-6)
     cb = TimingCallback()
 
-    callbacks = [early_stopping, model_checkpoint,  reduce_lr]
+    callbacks = [early_stopping, model_checkpoint, reduce_lr]
 
     model.summary()
     model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-    # Now initialise an iterator
-    #train_iterator = iter(train_dataset)
-    #val_iterator = iter(val_dataset)
-
-    # Create two objects, x & y, from batch
-    #train_x, train_y, train_weights =train_iterator.get_next()
-    #val_x, val_y, val_weights = val_iterator.get_next()
-
-    # ==================================================================================================================#
-    # Train Model
-    # ==================================================================================================================#
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+     Train Model
+     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     history = model.fit(train_dataset, epochs=100, callbacks=callbacks,
-                         validation_data=val_dataset, validation_freq=1, verbose=1, shuffle=True,
-                        workers=6, steps_per_epoch=len(training_batch_generator), use_multiprocessing=True
+                        validation_data=val_dataset, validation_freq=1, verbose=1, shuffle=True,
+                        workers=6, steps_per_epoch=len(training_batch_generator), use_multiprocessing=True,
                         )
+
+    # Save results
+    with open('data\\history.pkl', 'wb') as filehandle:
+        # store the data as binary data stream
+        pickle.dump(history, filehandle)
 
     #max_queue_size=6, workers=6,  tf.data.AUTOTUNE,
 