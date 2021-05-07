8c709a0cdc777bb865caa318d307893d589667f6
bewilson <benjamin.james.wilson@cern.ch>
2021-05-07 17:47:31 +0200

    corrections to files.py

--- files.py
+++ files.py
@@ -1,30 +1,33 @@
 """
 Just file definitions
+Note: If no files are appearing check that the '/' in the glob statements is NOT a '\\' (This is a side effect from development in Windows)
 """
 
 import glob as glob
 
-ntuple_dir = "E:\\NTuples\\TauClassifier"
+ntuple_dir = "/eos/user/b/bewilson/TauClassifier/NTuples"
 tree = "tree"
 
-training_files_dictionary = {"Gammatautau": glob.glob(f"{ntuple_dir}\\*Gammatautau*\\*.root")[:-1],
-                             "JZ1": glob.glob(f"{ntuple_dir}\\*JZ1*\\*.root")[:-1],
-                             "JZ2": glob.glob(f"{ntuple_dir}\\*JZ2*\\*.root")[:-1],
-                             "JZ3": glob.glob(f"{ntuple_dir}\\*JZ3*\\*.root")[:-1],
-                             "JZ4": glob.glob(f"{ntuple_dir}\\*JZ4*\\*.root")[:-1],
-                             "JZ5": glob.glob(f"{ntuple_dir}\\*JZ5*\\*.root")[:-1],
-                             "JZ6": glob.glob(f"{ntuple_dir}\\*JZ6*\\*.root")[:-1],
-                             "JZ7": glob.glob(f"{ntuple_dir}\\*JZ7*\\*.root")[:-1],
-                             "JZ8": glob.glob(f"{ntuple_dir}\\*JZ8*\\*.root")[:-1],
+training_files_dictionary = {"Gammatautau": glob.glob(f"{ntuple_dir}/*Gammatautau*/*.root")[:-1],
+                             "JZ1": glob.glob(f"{ntuple_dir}/*JZ1*/*.root")[:-1],
+                             "JZ2": glob.glob(f"{ntuple_dir}/*JZ2*/*.root")[:-1],
+                             "JZ3": glob.glob(f"{ntuple_dir}/*JZ3*/*.root")[:-1],
+                             "JZ4": glob.glob(f"{ntuple_dir}/*JZ4*/*.root")[:-1],
+                             "JZ5": glob.glob(f"{ntuple_dir}/*JZ5*/*.root")[:-1],
+                             "JZ6": glob.glob(f"{ntuple_dir}/*JZ6*/*.root")[:-1],
+                             "JZ7": glob.glob(f"{ntuple_dir}/*JZ7*/*.root")[:-1],
+                             "JZ8": glob.glob(f"{ntuple_dir}/*JZ8*/*.root")[:-1],
                              }
 
-validation_files_dictionary = {"Gammatautau": glob.glob(f"{ntuple_dir}\\*Gammatautau*\\*.root")[-1:],
-                                "JZ1": glob.glob(f"{ntuple_dir}\\*JZ1*\\*.root")[-1:],
-                                "JZ2": glob.glob(f"{ntuple_dir}\\*JZ2*\\*.root")[-1:],
-                                "JZ3": glob.glob(f"{ntuple_dir}\\*JZ3*\\*.root")[-1:],
-                                "JZ4": glob.glob(f"{ntuple_dir}\\*JZ4*\\*.root")[-1:],
-                                "JZ5": glob.glob(f"{ntuple_dir}\\*JZ5*\\*.root")[-1:],
-                                "JZ6": glob.glob(f"{ntuple_dir}\\*JZ6*\\*.root")[-1:],
-                                "JZ7": glob.glob(f"{ntuple_dir}\\*JZ7*\\*.root")[-1:],
-                                "JZ8": glob.glob(f"{ntuple_dir}\\*JZ8*\\*.root")[-1:],
+validation_files_dictionary = {"Gammatautau": glob.glob(f"{ntuple_dir}/*Gammatautau*/*.root")[-1:],
+                                "JZ1": glob.glob(f"{ntuple_dir}/*JZ1*/*.root")[-1:],
+                                "JZ2": glob.glob(f"{ntuple_dir}/*JZ2*/*.root")[-1:],
+                                "JZ3": glob.glob(f"{ntuple_dir}/*JZ3*/*.root")[-1:],
+                                "JZ4": glob.glob(f"{ntuple_dir}/*JZ4*/*.root")[-1:],
+                                "JZ5": glob.glob(f"{ntuple_dir}/*JZ5*/*.root")[-1:],
+                                "JZ6": glob.glob(f"{ntuple_dir}/*JZ6*/*.root")[-1:],
+                                "JZ7": glob.glob(f"{ntuple_dir}/*JZ7*/*.root")[-1:],
+                                "JZ8": glob.glob(f"{ntuple_dir}/*JZ8*/*.root")[-1:],
                                 }
+
+print(training_files_dictionary)
