3f6159cef3f5bb3660b6cc5f5aaa3bd2b7f15d68
Ben Wilson <benjamin.james.wilson@cern.ch>
2022-04-08 15:44:23 +0200

    Added ROC and confusion matrix

--- visualise.py
+++ visualise.py
@@ -1,10 +1,16 @@
 import uproot
+import glob
 from source2 import plotting_functions as pf
 
 def visualise():
 
+    all_data = uproot.concatenate(glob.glob("results/*.root"), library='np')
+    y_true = all_data["TauClassifier_TruthScores"]
+    y_pred = all_data["TauClassifier_Scores"]
+    weights = all_data["TauClassifier_Weight"]
     
-
+    pf.plot_ROC(y_true[:, 0], y_pred[:, 0], weights)
+    pf.plot_confusion_matrix(y_true, y_pred, weights=weights)
 
 if __name__ == "__main__":
     visualise()
