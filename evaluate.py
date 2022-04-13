6e8b48e019dc7403d8f0f797b9c953c4cfa41cf1
Ben Wilson <benjamin.james.wilson@cern.ch>
2022-04-13 14:00:48 +0200

    removed unused imports

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,10 @@ import os
 import tqdm 
 import glob
 import yaml
+import ray
 from sklearn.model_selection import train_test_split
 from model.models import ModelDSNN
-from source.dataloader import DataWriter
-
+from source.datawriter import RayDataWriter, DataWriter
 
 def evaluate():
 
@@ -29,15 +29,28 @@ def evaluate():
     model.load_weights(weights_file)
 
     # TODO: see if this can be parallelised with ray (May not work due to how fussy tf can be with model objects)
+    # TODO: ^^^ Cannot pass model object to ray Actor - have to create model on Actor instantiation
     # Write results to file
-    for i, file in tqdm.tqdm(enumerate(tau_test_files)):
-        loader = DataWriter(file, "config/features.yaml", batch_size=1000, step_size=10000)
+
+    for i, file in tqdm.tqdm(enumerate(tau_test_files), total=len(tau_test_files)):
+        loader = DataWriter(file, "config/features.yaml")
         loader.write_results(model, output_file=f"results/taus_{i:02d}.root")
-    
-    for i, file in tqdm.tqdm(enumerate(jet_test_files)):
-        loader = DataWriter(file, "config/features.yaml", batch_size=1000, step_size=10000)
+
+    for i, file in tqdm.tqdm(enumerate(jet_test_files), total=len(jet_test_files)):
+        loader = DataWriter(file, "config/features.yaml")
         loader.write_results(model, output_file=f"results/jets_{i:02d}.root")
 
+    # results = []
+    # for i, file in tqdm.tqdm(enumerate(tau_test_files)):
+    #     loader = RayDataWriter.remote(file, "config/features.yaml")
+    #     results.append(loader.write_results.remote(model, output_file=f"results/taus_{i:02d}.root"))
+    # ray.get(results)
+
+    # results = []
+    # for i, file in tqdm.tqdm(enumerate(jet_test_files)):
+    #     loader = RayDataWriter.remote(file, "config/features.yaml")
+    #     results.append(loader.write_results.remote(model, output_file=f"results/jets_{i:02d}.root"))
+    # ray.get(results)
 
 if __name__ == "__main__":
     evaluate()
