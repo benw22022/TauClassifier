d9199b6cb1279027c01eaeebe5e7ca96f782510e
Ben Wilson <benjamin.james.wilson@cern.ch>
2022-04-29 14:27:25 +0200

    dont need

--- run_valgrind.sh
+++
@@ -1,8 +0,0 @@
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         ../miniconda3/envs/tauid/bin/python3 tauclassifier.py train | tee valgrind_log.txt
-
