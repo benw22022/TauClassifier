3a4a65a9e8136f70fcf6d5fc5d57fc40c52118b1
Ben Wilson <benjamin.james.wilson@cern.ch>
2022-05-02 14:47:03 +0200

    tf logging level can be changed

--- tauclassifier.py
+++ tauclassifier.py
@@ -50,6 +50,8 @@ def unified_tau_classifier(config: DictConfig) -> None:
     # Setup enviroment from config
     if not config.use_gpu:
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = config.tf_log_level 
+
 
     # Check that files can be found
     tau_files = glob.glob(config.TauFiles)
