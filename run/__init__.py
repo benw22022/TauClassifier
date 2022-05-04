5c6f8bbea458e0bf27dbdd2249296e2c6166005a
Ben Wilson <benjamin.james.wilson@cern.ch>
2022-05-04 10:09:28 +0200

    updated imports

--- run/__init__.py
+++ run/__init__.py
@@ -1,5 +1,5 @@
 from .train import train
 from .evaluate import evaluate
-from .visualise_mk2 import visualise
+from .visualise import visualise
 from .plot_output import make_control_plots
 from .feature_ranking import feature_rank
